3387a0c0448aa1197b3b43ffa156f89cf31e0573
oconnoranson-debug <oconnor.anson@gmail.com>
2026-07-28 15:14:07 -0500

    Extract decision question workspace component

---
+++ client/src/components/DecisionQuestion.jsx
@@ -0,0 +1,10 @@
+function DecisionQuestion({ question }) {
+  return (
+    <section className="decision-section">
+      <h3>Decision Question</h3>
+      <p>{question}</p>
+    </section>
+  );
+}
+
+export default DecisionQuestion;
